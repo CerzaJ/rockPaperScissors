5109b2c57e36a6e0ecf7337a1b95e434c2fcf3e5
CerzaJ <142043589+CerzaJ@users.noreply.github.com>
2023-09-05 07:16:33 -0600

    Add function to determine a winner or a looser for each round

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,4 +1,30 @@
 const possibleChoices = ['Rock', 'Paper', 'Scissors']
+
 function getComputerChoice(){
-const randomChoice = possibleChoices[Math.floor(Math.random()*possibleChoices.length)];
+    const randomChoice = possibleChoices[Math.floor(Math.random()*possibleChoices.length)];
+    return(randomChoice);
 }
+
+function playRound(playerSelection, computerSelection){
+    const rock = 'rock'
+    const paper = 'paper'
+    const scissors = 'scissors'
+    if(playerSelection == computerSelection){
+        return('Tie')
+    }else if(playerSelection == rock && computerSelection == paper){
+        return('You loose, paper beats rock')
+    }else if(playerSelection == scissors && computerSelection == paper){
+        return('You won')   
+    }else if(playerSelection == rock && computerSelectionn == scissors){
+        return('You won')
+    }else if(playerSelection == paper && computerSelection == scissors){
+        return('You loose, scissors beats paper')
+    }else if(playerSelection == paper && computerSelection == rock){
+        return('You won')
+    }else if (playerSelection == scissors && computerSelection == rock){
+        return('You loose, rock beats scissors')
+    }
+}
+const playerSelection = prompt('Choose R P S')
+const computerSelection = getComputerChoice()
+console.log(playRound(playerSelection.toLowerCase(), computerSelection.toLowerCase()))
