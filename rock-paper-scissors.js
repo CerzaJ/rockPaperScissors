22260e904e353a51c4e48e7a69b41e3287176584
CerzaJ <142043589+CerzaJ@users.noreply.github.com>
2023-09-11 17:29:40 -0600

    Change the conditional operators to make them concise, switch the return after the round for an array

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,30 +1,47 @@
-const possibleChoices = ['Rock', 'Paper', 'Scissors']
-
-function getComputerChoice(){
-    const randomChoice = possibleChoices[Math.floor(Math.random()*possibleChoices.length)];
-    return(randomChoice);
+function getCompChoice(){
+    const possibleChoices = ['rock', 'paper', 'scissors'];
+    let selection = possibleChoices[Math.floor(Math.random()*possibleChoices.length)];
+    return(selection);
 }
 
-function playRound(playerSelection, computerSelection){
-    const rock = 'rock'
-    const paper = 'paper'
-    const scissors = 'scissors'
-    if(playerSelection == computerSelection){
-        return('Tie')
-    }else if(playerSelection == rock && computerSelection == paper){
-        return('You loose, paper beats rock')
-    }else if(playerSelection == scissors && computerSelection == paper){
-        return('You won')   
-    }else if(playerSelection == rock && computerSelectionn == scissors){
-        return('You won')
-    }else if(playerSelection == paper && computerSelection == scissors){
-        return('You loose, scissors beats paper')
-    }else if(playerSelection == paper && computerSelection == rock){
-        return('You won')
-    }else if (playerSelection == scissors && computerSelection == rock){
-        return('You loose, rock beats scissors')
+let result = [];
+function playRound(playerChoice, computerChoice){   
+    let roundResult = ''; 
+    console.log(computerChoice);
+    console.log(playerChoice);
+    if(playerChoice == computerChoice){
+        roundResult += 'It was a tie ';
+    }else if(
+        (playerChoice == 'rock' && computerChoice == 'scissors')||
+        (playerChoice == 'paper' && computerChoice == 'rock')||
+        (playerChoice == 'scissors' && computerChoice == 'paper')
+    ){
+        roundResult += 'You won ';
+    }else{
+        roundResult += 'You lost ';
+    }
+    result.push(`Round ${roundNum + 1}: ${roundResult}`);
+    roundNum++;
+}
+
+let computerChoice;
+let playerChoice;
+let roundNum = 0;
+let compScore = 0;
+let playerScore = 0;
+
+function game(){
+    for(let i = 0; i < 5; i++){
+    let playerChoice= prompt ('R, P, S').toLowerCase();
+    let computerChoice = getCompChoice();
+    console.log(playRound(playerChoice, computerChoice));
     }
 }
-const playerSelection = prompt('Choose R P S')
-const computerSelection = getComputerChoice()
-console.log(playRound(playerSelection.toLowerCase(), computerSelection.toLowerCase()))
+
+function getRoundResults(){
+    for(let i = 0; i < result.length; i++){
+        console.log(result[i])
+    }
+}
+
+
